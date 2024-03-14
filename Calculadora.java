474086cbbcdbbaa4b93488cac7a0c6a6cc938804
Afonso Scrivani <afonso_a93@hotmail.com>
2024-03-14 16:01:08 -0300

    feat(ap006): implementa a calculadora
    - faz soma
    - faz subtração
    - faz multiplicação
    - faz divisão
    - o intuito era estudar métodos

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+    //tipo de retorno
+    //nome
+    //lista de parâmetros
+    //corpo
+    int somar(int n1, int n2) {
+        int res = n1 + n2;
+        return res;
+    }
+    int subtrair(int n1, int n2) {
+        return n1 - n2;
+    }
+    int multiplicar(int n1, int n2) {
+        return n1 * n2;
+    }
+    int dividir(int n1, int n2) {
+        return n1 / n2;
+    }
+}
