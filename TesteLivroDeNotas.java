36db009309cfe290ba0179117ef4d97404be6af4
Afonso Scrivani <afonso_a93@hotmail.com>
2024-03-14 16:11:42 -0300

    test(ap006): testa usando o construtor padrao

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class TesteLivroDeNotas {
+    public static void main(String[] args)  {
+        //var livroDeNotas = new LivroDeNotas();
+        //var livroDeNotas2 = new LivroDeNotas();
+        String nomeDaDisciplina = JOptionPane.
+        showInputDialog("Prof, qual o nome da disciplina?"
+        );
+        //livroDeNotas.nomeDaDisciplina = nomeDaDisciplina;
+        nomeDaDisciplina = JOptionPane.showInputDialog(
+            "Prof, qual o nome da disciplina 2?"
+            );
+        //livroDeNotas2.nomeDaDisciplina = nomeDaDisciplina;
+        //livroDeNotas.exibirMensagem();
+        //livroDeNotas2.exibirMensagem();
+    }   
+}
